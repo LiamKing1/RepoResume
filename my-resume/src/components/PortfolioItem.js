80fe3e3b524307834a9f3505d91a94d484ceb8cc
Liam King <techy.willyk3@gmail.com>
2023-10-22 09:59:05 -0500

    Inserted a photo to use as project photo until further notice.

--- my-resume/src/components/PortfolioItem.js
+++ my-resume/src/components/PortfolioItem.js
@@ -1,12 +1,13 @@
 import Card from "../style/Card";
 import styleClass from "../style/Card.module.css";
+import Beltloader from "./BeltLoader.jpg";
 
 function PortfolioItem(props) {
   return (
     <div>
       <Card>
         <div>
-          <img src={props.image} alt={props.filename}></img>
+          <img className={styleClass.img} src={Beltloader} alt={props.filename}></img>
         </div>
         <div>
           <h1> {props.filename} </h1>
